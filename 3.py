a4bc057efa43d90af54545b81d9aa984c7675168
ibrahimfakhry <Ibrahim fakhrey@github.com>
2024-05-29 14:19:35 +0300

    ddasdasdasdasd

--- 3.py
+++ 3.py
@@ -1,18 +1,32 @@
 names=["ali","ahmed","mona"]
 passwords=["123","0000",""]
 print("welcom to our programme please write down your information to see our secret ")
-x= input("please enter your user_name\n")
+
 def search(x):
     for i in range (len(names)):
         if names[i]==x:
-            return i
-if search(x):
-
-    if passwords[search(x)]=="":
-        new_password=input("you did not assigne password for this account write the new password ")
-    else:
-        y = input("please enter the password ")
-        if passwords[search(x)] ==y:
-            print("our sescret that ibrahim donot like the fish ")
+         return i
+x= ""
+while True:
+    descision=input("please tell me what you want to make change the password press 1 or login press 2 \n")
+    if descision=="2":
+        x = input("please enter your user_name\n")
+        index=search(x)
+        if passwords[index]=="":
+            new_password=input("this is an empty password please write a password  ")
+            passwords[index]=new_password
         else:
-            print("wrong password ")
+            y= input("please enter the password ")
+            if y ==passwords[index]:
+                 print("the secret is one ")
+            else:
+                print("wrong password ")
+    else:
+        user=input("enter your user_name ")
+        i=search(user)
+        y=passwords[i]
+        n=input("please enter your old password \n")
+        if n ==y:
+            new=input("write the new password ")
+            passwords[i]=new
+
